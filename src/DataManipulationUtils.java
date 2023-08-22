b68e20d9f25bf31ac51e55b5d4e334e7709af1d7
difelk <dilshan.f@theavo.net>
2023-08-22 21:19:13 +0530

    created new class to convertion

---
+++ src/DataManipulationUtils.java
@@ -0,0 +1,15 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DataManipulationUtils {
+
+    public LocalDateTime ConvertDateStringToLocalDateTime(String dateTimeString, String formatPattern) {
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(formatPattern);
+            return LocalDateTime.parse(dateTimeString, formatter);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Invalid date and time format.");
+        }
+    }
+}
