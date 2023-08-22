5ac4b2efa40e7ced2aa0ffcebfcabd6e9d4bde9f
difelk <dilshan.f@theavo.net>
2023-08-22 17:00:17 +0530

    folder structure

---
+++ src/Category.java
@@ -0,0 +1,12 @@
+public class Category {
+    private String name;
+
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
