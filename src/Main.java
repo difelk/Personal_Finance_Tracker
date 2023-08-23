89c72251d72754049d5f4f7bb77cbe2abb7639ae
difelk <dilshan.f@theavo.net>
2023-08-23 11:54:34 +0530

    added packages

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,5 @@ import transaction.TransactionLinkedList;
 public class Main {
     public static void main(String[] args) {
         TransactionLinkedList transactionLinkedList = new TransactionLinkedList();
-
     }
 }
