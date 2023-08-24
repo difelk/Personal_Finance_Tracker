5e52c38550ada478e7e5b9915ef93c46146a1b85
ilmeedesilva <ilmeedesilva@gmail.com>
2023-08-24 10:31:36 +0530

    CategoryNode, CategoryLinkedList, addCategory creation

--- src/Category/CategoryLinkedList.java
+++ src/Category/CategoryLinkedList.java
@@ -1,4 +1,23 @@
 package Category;
 
 public class CategoryLinkedList {
+    public CategoryNode head;
+
+    public void addCategory(Category data){
+        CategoryNode newCategoryNode = new CategoryNode(data);
+
+        if(!isEmpty()) {
+            newCategoryNode.setNextRef(this.head);
+        }
+        this.head = newCategoryNode;
+
+        CategoryNode currentNode = head;
+        while(currentNode.next != null){
+            currentNode = currentNode.next;
+        }
+
+    }
+    public boolean isEmpty(){
+        return this.head == null;
+    }
 }
